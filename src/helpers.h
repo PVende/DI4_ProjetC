874f12339f81e34741954f1d1e8f1940b1ba352c
Pierre Vende <21400556t@univ-tours.local>
2017-10-04 12:13:44 +0200

    Bug fixes

--- src/helpers.h
+++ src/helpers.h
@@ -5,7 +5,7 @@
 #ifndef DI4_PROJETC_HELPERS_H
 #define DI4_PROJETC_HELPERS_H
 
-#define MALLOC(varname, type, size) type* varname = (type*) malloc((size) * sizeof(type)); \
+#define MALLOC(varname, type, size) varname = (type*) malloc((size) * sizeof(type)); \
                                     if(varname == NULL) \
                                         fatalError("Malloc failed");
 
--- tests/TestsRunner.h
+++ tests/TestsRunner.h
@@ -2,6 +2,13 @@
 #ifndef TESTRUNNER_H
 #define TESTRUNNER_H
 
+#include "ConfigTests.h"
+#include "BatchListTests.h"
+#include "SequenceTests.h"
+#include "SolutionTests.h"
+#include "BatchTests.h"
+#include "InstanceTests.h"
+
 void TestRunner_runTests();
 
 #endif // TESTRUNNER_H
